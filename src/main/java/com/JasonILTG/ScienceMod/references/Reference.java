314437254c3d896f620de28cb6afe18633be999a
Jack <Jack@SYY_SP3>
2015-11-20 00:51:39 -0800

    Miscellaneous updates

--- src/main/java/com/JasonILTG/ScienceMod/references/Reference.java
+++ src/main/java/com/JasonILTG/ScienceMod/references/Reference.java
@@ -1,6 +1,5 @@
 package com.JasonILTG.ScienceMod.references;
 
-import java.util.ArrayList;
 
 public class Reference
 {
@@ -8,5 +7,5 @@ public class Reference
 	public static final String MOD_NAME = "Science Mod";
 	public static final String VERSION = "1.0";
 	public static final String CLIENT_PROXY_CLASS = "com.JasonILTG.ScienceMod.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "com.JasonILTG.ScienceMod.proxy.CommonProxy";
+	public static final String SERVER_PROXY_CLASS = "com.JasonILTG.ScienceMod.proxy.ServerProxy";
 }
