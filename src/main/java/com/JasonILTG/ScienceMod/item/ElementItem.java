69aba0ebf392d382a4e6de3e565d85b05ce21cd0
Jack <Jack@SYY_SP3>
2015-11-19 21:12:36 -0800

    Updates to item hierarchy

--- src/main/java/com/JasonILTG/ScienceMod/item/ElementItem.java
+++ src/main/java/com/JasonILTG/ScienceMod/item/ElementItem.java
@@ -1,9 +1,8 @@
 package com.JasonILTG.ScienceMod.item;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 
-public class ElementItem extends Item
+public class ElementItem extends ItemScience
 {
 	public ElementItem()
 	{
---
+++ src/main/java/com/JasonILTG/ScienceMod/item/ItemScience.java
@@ -0,0 +1,12 @@
+package com.JasonILTG.ScienceMod.item;
+
+import net.minecraft.item.Item;
+
+public class ItemScience extends Item
+{
+	public ItemScience()
+	{
+		super();
+	}
+	
+}
