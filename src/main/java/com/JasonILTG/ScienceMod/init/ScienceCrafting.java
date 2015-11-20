b3982035fe6d4417737d528224bf54716c73a1de
Jack <Jack@SYY_SP3>
2015-11-20 12:47:50 -0800

    Temporary recipe for electrolyzer

--- src/main/java/com/JasonILTG/ScienceMod/init/ScienceCrafting.java
+++ src/main/java/com/JasonILTG/ScienceMod/init/ScienceCrafting.java
@@ -1,6 +1,7 @@
 package com.JasonILTG.ScienceMod.init;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -15,5 +16,12 @@ public class ScienceCrafting
 			"OOO",
 			'O', Blocks.glass_pane, 'X', Blocks.planks
 		});
+		
+		GameRegistry.addRecipe(new ItemStack(ScienceModBlocks.electrolyzer),
+			"III",
+			"RWR",
+			"III",
+			'I', Items.iron_ingot, 'R', Items.redstone, 'W', Items.water_bucket
+			);
 	}
 }
