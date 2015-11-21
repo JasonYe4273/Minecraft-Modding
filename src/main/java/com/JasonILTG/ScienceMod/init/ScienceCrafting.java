66107aa95f4af358928026f0750418cb81e5ab6d
Jason <jasoniltg@gmail.com>
2015-11-21 00:11:47 -0800

    Fixed JSON and Added Element to Jar Recipe

--- src/main/java/com/JasonILTG/ScienceMod/init/ScienceCrafting.java
+++ src/main/java/com/JasonILTG/ScienceMod/init/ScienceCrafting.java
@@ -1,5 +1,6 @@
 package com.JasonILTG.ScienceMod.init;
 
+import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -9,13 +10,12 @@ public class ScienceCrafting
 {
 	public static void init()
 	{
-		GameRegistry.addRecipe(new ItemStack(ScienceItems.jar, 4), new Object[]
-		{
+		GameRegistry.addRecipe(new ItemStack(ScienceItems.jar, 4),
 			" X ",
 			"O O",
 			"OOO",
 			'O', Blocks.glass_pane, 'X', Blocks.planks
-		});
+			);
 		
 		GameRegistry.addRecipe(new ItemStack(ScienceModBlocks.electrolyzer),
 			"III",
@@ -23,5 +23,9 @@ public class ScienceCrafting
 			"III",
 			'I', Items.iron_ingot, 'R', Items.redstone, 'W', Items.water_bucket
 			);
+		for( int meta = 0; meta < ScienceItems.element.getNumSubtypes(); meta++ )
+		{
+			GameRegistry.addShapelessRecipe(new ItemStack(ScienceItems.jar, 1), new ItemStack(ScienceItems.element, 1, meta));
+		}
 	}
 }
