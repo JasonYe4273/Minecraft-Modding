c44810f2c9872b4b21e794a35f27bae48ecf1c17
Jack <Jack@SYY_SP3>
2015-11-20 17:59:19 -0800

    Removed TemplateMod

--- src/main/java/com/JasonILTG/Template/reference/Reference.java
+++
@@ -1,8 +0,0 @@
-package com.JasonILTG.Template.reference;
-
-public class Reference
-{
-	public static final String MOD_ID = "TemplateMod";
-	public static final String MOD_NAME = "Template Mod";
-	public static final String VERSION = "1.0";
-}
