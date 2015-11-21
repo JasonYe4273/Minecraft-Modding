69ebe840549a79b91b0d88467223abde16b75d30
Jason <jasoniltg@gmail.com>
2015-11-20 23:47:01 -0800

    Rows 3 and 4

--- src/main/java/com/JasonILTG/ScienceMod/references/Names.java
+++ src/main/java/com/JasonILTG/ScienceMod/references/Names.java
@@ -5,7 +5,7 @@ public class Names
 	public static final class Items
 	{
 		public static final String ELEMENT = "element";
-		public static final String[] ELEMENT_SUBTYPES = {"hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "nitrogen", "oxygen", "fluorine", "neon"};
+		public static final String[] ELEMENT_SUBTYPES = {"hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "nitrogen", "oxygen", "fluorine", "neon", "sodium", "magnesium", "aluminium", "silicon", "phosphorous", "sulfur", "chlorine", "argon", "potassium", "calcium", "scandium", "titanium", "vanadium", "manganese", "iron", "cobalt", "nickel", "copper", "zinc", "gallium", "germanium", "arsenic", "selenium", "bromine", "krypton"};
 	}
 	
 	public static final class Blocks
