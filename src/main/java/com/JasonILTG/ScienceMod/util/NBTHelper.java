43a043ccbf4a69051d0ecb744de3efd82b86560b
Jack <Jack@SYY_SP3>
2015-11-20 18:47:35 -0800

    Finishing experimental methods

--- src/main/java/com/JasonILTG/ScienceMod/util/NBTHelper.java
+++ src/main/java/com/JasonILTG/ScienceMod/util/NBTHelper.java
@@ -8,21 +8,48 @@ import com.JasonILTG.ScienceMod.references.NBTKeys;
 
 public class NBTHelper
 {
-	public static ItemStack[] readItemStackFromNBT(NBTTagCompound tag, int inventorySize)
+	/**
+	 * Experimental method for reading an array of ItenStack from a compound tag and putting them into an inventory.
+	 * 
+	 * @param tag the tag to read from
+	 * @param inventory the inventory to place items into
+	 */
+	public static void readItemStackFromNBT(NBTTagCompound tag, ItemStack[] inventory)
 	{
 		NBTTagList tagList = tag.getTagList(NBTKeys.ITEMS, 10);
-		ItemStack[] stack = new ItemStack[inventorySize];
 		
-		for (int i = 0; i < tagList.tagCount(); ++i)
+		for (int i = 0; i < tagList.tagCount(); i++)
 		{
 			NBTTagCompound tagCompound = tagList.getCompoundTagAt(i);
 			byte slotIndex = tagCompound.getByte("Slot");
-			if (slotIndex >= 0 && slotIndex < stack.length)
+			if (slotIndex >= 0 && slotIndex < inventory.length)
 			{
-				stack[slotIndex] = ItemStack.loadItemStackFromNBT(tagCompound);
+				inventory[slotIndex] = ItemStack.loadItemStackFromNBT(tagCompound);
+			}
+		}
+	}
+	
+	/**
+	 * Experimental method for saving an inventory into an NBT tag.
+	 * 
+	 * @param inventory the inventory of the current tile entity
+	 * @param tag the tag to write the inventory into
+	 */
+	public static void addInventoryToCompoundTag(ItemStack[] inventory, NBTTagCompound tag)
+	{
+		NBTTagList tagList = new NBTTagList();
+		
+		for (int currentIndex = 0; currentIndex < inventory.length; currentIndex++)
+		{
+			if (inventory[currentIndex] != null)
+			{
+				NBTTagCompound tagCompound = new NBTTagCompound();
+				tagCompound.setByte("Slot", (byte) currentIndex);
+				inventory[currentIndex].writeToNBT(tagCompound);
+				tagList.appendTag(tagCompound);
 			}
 		}
 		
-		return stack;
+		tag.setTag(NBTKeys.ITEMS, tagList);
 	}
 }
