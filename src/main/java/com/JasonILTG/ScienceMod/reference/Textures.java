b302724fd6ed2bbf4c3aa7999fc29159a9f3c89b
Jack <Jack@SYY_SP3>
2015-11-23 15:59:30 -0800

    Minor changes

--- src/main/java/com/JasonILTG/ScienceMod/reference/Textures.java
+++ src/main/java/com/JasonILTG/ScienceMod/reference/Textures.java
@@ -6,10 +6,12 @@ import com.JasonILTG.ScienceMod.util.ResourceHelper;
 
 public class Textures
 {
+	private static final String TEXTURE_LOCATION = "textures/";
 	
 	public static final class GUI
 	{
-		private static final String TEXTURE_LOCATION = "textures/gui/";
-		public static final ResourceLocation ELECROLYZER = ResourceHelper.getResourceLocation(TEXTURE_LOCATION + "electrolyzer.png");
+		private static final String GUI_TEXTURE_LOCATION = TEXTURE_LOCATION + "gui/";
+		public static final ResourceLocation ELECROLYZER = ResourceHelper.getResourceLocation(GUI_TEXTURE_LOCATION
+				+ "electrolyzer.png");
 	}
 }
