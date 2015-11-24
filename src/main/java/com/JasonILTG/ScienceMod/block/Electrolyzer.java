ab17a9b06cd505f900167b98ed7250dbc9b912a6
Jack <Jack@SYY_SP3>
2015-11-23 21:32:03 -0800

    Player can now insert water into the electrolyzer using buckets

--- src/main/java/com/JasonILTG/ScienceMod/block/Electrolyzer.java
+++ src/main/java/com/JasonILTG/ScienceMod/block/Electrolyzer.java
@@ -1,11 +1,13 @@
 package com.JasonILTG.ScienceMod.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -37,27 +39,26 @@ public class Electrolyzer extends MachineScience
 		return 3;
 	}
 	
-	// The super method for this doesn't do anything, so we can overrride safely.
-	/**
-	 * When right clicked on by a water bucket, it filles with 1000 mB of water.
-	 */
 	@Override
-	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn)
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX,
+			float hitY, float hitZ)
 	{
 		// If the player is holding a water bucket and if the block has a tile entity electrolyzer
-		if (playerIn.inventory.getCurrentItem().isItemEqual(new ItemStack(Items.water_bucket)))
+		if (player.inventory.getCurrentItem().isItemEqual(new ItemStack(Items.water_bucket)))
 		{
-			TileEntity entity = worldIn.getTileEntity(pos);
+			TileEntity entity = world.getTileEntity(pos);
 			if (entity instanceof TEElectrolyzer)
 			{
 				TEElectrolyzer tileElectrolyzer = (TEElectrolyzer) entity;
 				
 				// If the tank is successfully filled, change the bucket to empty.
 				if (tileElectrolyzer.fillAll(new FluidStack(FluidRegistry.WATER, 1000))) {
-					playerIn.inventory.getCurrentItem().setItem(Items.bucket);
+					player.inventory.getCurrentItem().setItem(Items.bucket);
+					return true;
 				}
-				
 			}
 		}
+		
+		return super.onBlockActivated(world, pos, state, player, side, hitX, hitY, hitZ);
 	}
 }
