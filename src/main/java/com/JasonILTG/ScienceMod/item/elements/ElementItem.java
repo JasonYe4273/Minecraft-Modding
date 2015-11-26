664dc9c67121af1efbfd3eabb6c47d2115520c6d
Jack <Jack@SYY_SP3>
2015-11-25 19:44:53 -0800

    Added enum for all elements

--- src/main/java/com/JasonILTG/ScienceMod/item/elements/ElementItem.java
+++ src/main/java/com/JasonILTG/ScienceMod/item/elements/ElementItem.java
@@ -3,18 +3,23 @@ package com.JasonILTG.ScienceMod.item.elements;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 import com.JasonILTG.ScienceMod.creativetabs.ScienceCreativeTabs;
 import com.JasonILTG.ScienceMod.init.ScienceModItems;
 import com.JasonILTG.ScienceMod.item.general.ItemScience;
 import com.JasonILTG.ScienceMod.reference.Names;
 import com.JasonILTG.ScienceMod.reference.Reference;
-
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.MathHelper;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ElementItem extends ItemScience
 {
@@ -39,7 +44,7 @@ public class ElementItem extends ItemScience
 	@SideOnly(Side.CLIENT)
 	public void getSubItems(Item item, CreativeTabs creativeTab, List list)
 	{
-		for (int meta = 0; meta < Names.Items.ELEMENT_SUBTYPES.length; meta++)
+		for (int meta = 0; meta < Names.Items.ELEMENT_SUBTYPES.length; meta ++)
 		{
 			list.add(new ItemStack(this, 1, meta));
 		}
@@ -55,11 +60,27 @@ public class ElementItem extends ItemScience
 	{
 		List<Item> itemList = new ArrayList<Item>();
 		
-		for (int meta = 0; meta < Names.Items.ELEMENT_SUBTYPES.length; meta++)
+		for (int meta = 0; meta < Names.Items.ELEMENT_SUBTYPES.length; meta ++)
 		{
 			itemList.add(new ItemStack(this, 1, meta).getItem());
 		}
 		
 		return itemList;
 	}
+	
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX,
+			float hitY, float hitZ)
+	{
+		// TODO Auto-generated method stub
+		return super.onItemUse(stack, playerIn, worldIn, pos, side, hitX, hitY, hitZ);
+	}
+	
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack)
+	{
+		// TODO Auto-generated method stub
+		return super.getItemUseAction(stack);
+	}
+	
 }
