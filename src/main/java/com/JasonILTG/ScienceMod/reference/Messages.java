c0b25cb6501503ccfd57850c6bb39fa85af0f9c4
Jason <jasoniltg@gmail.com>
2015-11-29 15:19:25 -0800

    Somehow Didn't Make it into the Last Commit

--- src/main/java/com/JasonILTG/ScienceMod/reference/Messages.java
+++ src/main/java/com/JasonILTG/ScienceMod/reference/Messages.java
@@ -5,4 +5,5 @@ public class Messages
 	public static final int TE_TANK_MESSAGE_ID = 1;
 	public static final int TE_PROGRESS_MESSAGE_ID = 2;
 	public static final int TE_MAX_PROGRESS_MESSAGE_ID = 3;
+	public static final int MIXER_SOLUTION_MESSAGE_ID = 4;
 }
