14a2fdb469baf2b571c7b3da40fa56c9fd69b447
Jason <jasoniltg@gmail.com>
2015-11-28 21:56:36 -0800

    Added Air Extractor Progress Bar

--- src/main/java/com/JasonILTG/ScienceMod/gui/AirExtractorGUI.java
+++ src/main/java/com/JasonILTG/ScienceMod/gui/AirExtractorGUI.java
@@ -1,10 +1,11 @@
 package com.JasonILTG.ScienceMod.gui;
 
-import net.minecraft.inventory.IInventory;
-
 import com.JasonILTG.ScienceMod.gui.general.InventoryGUI;
 import com.JasonILTG.ScienceMod.reference.Textures;
 import com.JasonILTG.ScienceMod.tileentity.general.TEInventory;
+import com.JasonILTG.ScienceMod.tileentity.general.TEMachine;
+
+import net.minecraft.inventory.IInventory;
 
 public class AirExtractorGUI extends InventoryGUI
 {
@@ -23,5 +24,13 @@ public class AirExtractorGUI extends InventoryGUI
 		this.drawTexturedModalRect(this.guiLeft + (Textures.GUI.DEFAULT_GUI_X_SIZE - Textures.GUI.AIR_EXTRACTOR_GUI_WIDTH) / 2,
 				this.guiTop,
 				0, 0, Textures.GUI.AIR_EXTRACTOR_GUI_WIDTH, Textures.GUI.AIR_EXTRACTOR_GUI_HEIGHT);
+		
+		TEMachine te = (TEMachine) container.getTE();
+		if (te != null)
+		{
+			drawPartial(Textures.GUI.PROGRESS_BAR_FULL, guiLeft + Textures.GUI.AIR_EXTRACTOR_PROGRESS_X, guiTop + Textures.GUI.AIR_EXTRACTOR_PROGRESS_Y,
+					Textures.GUI.DEFAULT_PROGRESS_WIDTH, Textures.GUI.DEFAULT_PROGRESS_HEIGHT, te.getCurrentProgress(), te.getMaxProgress(),
+					Textures.GUI.DEFAULT_PROGRESS_DIR, Textures.GUI.PROGRESS_BAR_EMPTY);
+		}
 	}
 }
