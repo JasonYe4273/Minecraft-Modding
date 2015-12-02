846961407be6fd49b296a62b286be189ca6c2100
Jack <Jack@SYY_SP3>
2015-12-02 15:55:32 -0800

    Renames

--- src/main/java/com/JasonILTG/ScienceMod/util/EntityHelper.java
+++ src/main/java/com/JasonILTG/ScienceMod/util/EntityHelper.java
@@ -6,13 +6,13 @@ import net.minecraft.world.World;
 
 import com.JasonILTG.ScienceMod.entity.projectile.ThrownChemical;
 import com.JasonILTG.ScienceMod.entity.projectile.ThrownElement;
-import com.JasonILTG.ScienceMod.item.elements.ElementItem;
+import com.JasonILTG.ScienceMod.item.elements.ItemElement;
 
 public class EntityHelper
 {
 	public static ThrownChemical getThrownEntity(ItemStack stack, World worldIn, EntityLivingBase thrower)
 	{
-		if (stack != null && stack.getItem() instanceof ElementItem) {
+		if (stack != null && stack.getItem() instanceof ItemElement) {
 			return new ThrownElement(worldIn, thrower, stack.getMetadata());
 		}
 		return null;
