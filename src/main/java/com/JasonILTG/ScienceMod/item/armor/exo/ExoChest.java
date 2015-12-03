f181cc4d672febd99d6e3b3e261b6ef115d1c4e7
Jack <Jack@SYY_SP3>
2015-12-03 10:34:59 -0800

    Changed my mind about exoskeleton again...

---
+++ src/main/java/com/JasonILTG/ScienceMod/item/armor/exo/ExoChest.java
@@ -0,0 +1,6 @@
+package com.JasonILTG.ScienceMod.item.armor.exo;
+
+public class ExoChest
+{
+	
+}
--- src/main/java/com/JasonILTG/ScienceMod/item/armor/exo/Exoskeleton.java
+++ src/main/java/com/JasonILTG/ScienceMod/item/armor/exo/Exoskeleton.java
@@ -1,13 +1,5 @@
 package com.JasonILTG.ScienceMod.item.armor.exo;
 
-import java.util.List;
-
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 import com.JasonILTG.ScienceMod.item.armor.ArmorScience;
 
 public class Exoskeleton extends ArmorScience
@@ -18,13 +10,4 @@ public class Exoskeleton extends ArmorScience
 	private static final String LEGGING_NAME = "legs";
 	private static final String BOOTS_NAME = "boots";
 	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void getSubItems(Item item, CreativeTabs creativeTab, List list)
-	{
-		for (int meta = 0; meta < 4; meta ++)
-		{
-			list.add(new ItemStack(this, 1, meta));
-		}
-	}
 }
