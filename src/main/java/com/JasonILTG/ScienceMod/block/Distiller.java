23858c66b5e129f000335e8e6e270a1f21ab4326
Jason <jasoniltg@gmail.com>
2015-12-05 18:45:57 -0800

    More Filter Removal

--- src/main/java/com/JasonILTG/ScienceMod/block/Distiller.java
+++ src/main/java/com/JasonILTG/ScienceMod/block/Distiller.java
@@ -1,5 +1,11 @@
 package com.JasonILTG.ScienceMod.block;
 
+import com.JasonILTG.ScienceMod.ScienceMod;
+import com.JasonILTG.ScienceMod.block.general.MachineScience;
+import com.JasonILTG.ScienceMod.reference.EnumGUI;
+import com.JasonILTG.ScienceMod.reference.Names;
+import com.JasonILTG.ScienceMod.tileentity.TEDistiller;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,18 +14,12 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
-import com.JasonILTG.ScienceMod.ScienceMod;
-import com.JasonILTG.ScienceMod.block.general.MachineScience;
-import com.JasonILTG.ScienceMod.reference.EnumGUI;
-import com.JasonILTG.ScienceMod.reference.Names;
-import com.JasonILTG.ScienceMod.tileentity.TEDistiller;
-
 public class Distiller extends MachineScience
 {
 	public Distiller()
 	{
 		super(Material.iron);
-		setUnlocalizedName(Names.Blocks.MACHINE_FILTER);
+		setUnlocalizedName(Names.Blocks.MACHINE_DISTILLER);
 	}
 	
 	@Override
