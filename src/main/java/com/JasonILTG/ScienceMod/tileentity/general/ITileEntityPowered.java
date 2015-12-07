1c23a5f98ce6350bb7a75a5099c33500091849da
Jason <jasoniltg@gmail.com>
2015-12-06 22:33:31 -0800

    Forgot to Save

--- src/main/java/com/JasonILTG/ScienceMod/tileentity/general/ITileEntityPowered.java
+++ src/main/java/com/JasonILTG/ScienceMod/tileentity/general/ITileEntityPowered.java
@@ -10,22 +10,12 @@ import com.JasonILTG.ScienceMod.manager.IPowered;
 public interface ITileEntityPowered extends IPowered
 {
 	/**
-<<<<<<< HEAD
-	 * Returns whether this tile entity has sufficient power.
-	 * 
-	 * @return Whether this tile entity has sufficient power
-=======
 	 * @return Whether the machine has sufficient power to continue operation.
->>>>>>> 13179d3d334219dc06e91ba409879ec9f42a7f0e
 	 */
 	public boolean hasPower();
 	
 	/**
-<<<<<<< HEAD
-	 * Does everything that must be done for this tile entity with regards to power.
-=======
 	 * The action to do if the machine has sufficient power.
->>>>>>> 13179d3d334219dc06e91ba409879ec9f42a7f0e
 	 */
 	public void powerAction();
 }
