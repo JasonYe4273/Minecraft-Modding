5c9ef22a26d40d95a39537c2d092fcbee9476365
Jack <Jack@10.63.116.210>
2015-12-11 12:49:32 -0800

    Adding a few more ions

--- src/main/java/com/JasonILTG/ScienceMod/reference/chemistry/Anion.java
+++ src/main/java/com/JasonILTG/ScienceMod/reference/chemistry/Anion.java
@@ -5,13 +5,17 @@ public enum Anion implements Ion
 	// Single atom anions
 	NITRIDE(Element.NITROGEN, "Nitride", -3), OXIDE(Element.OXYGEN, "Oxide", -2), FLUORIDE(Element.FLUORINE, "Fluoride", -1),
 	PHOSPHIDE(Element.PHOSPHORUS, "Phosphide", -3), SULFIDE(Element.SULFUR, "Sulfide", -2), CHLORIDE(Element.CHLORINE, "Chloride", -1),
-	BROMIDE(Element.BROMINE, "Bromide", -1), IODIDE(Element.IODINE, "Iodide", -1)
+	BROMIDE(Element.BROMINE, "Bromide", -1), IODIDE(Element.IODINE, "Iodide", -1),
 	// Polyatomic ions
-	;
+	HYDROXIDE(new Element[] { Element.OXYGEN, Element.HYDROGEN }, new int[] { 1, 1 }, "Hydroxide", -1),
+	CYANIDE(new Element[] { Element.CARBON, Element.NITROGEN }, new int[] { 1, 1 }, "Cyanide", -1),
+	ACETATE(new Element[] { Element.CARBON, Element.HYDROGEN, Element.OXYGEN }, new int[] { 2, 3, 2 }, "Acetate", -1),
+	CHROMATE(new Element[] { Element.CHROMIUM, Element.OXYGEN }, new int[] { 1, 4 }, "Chromate", -2);
 	
 	private AtomGroup base;
 	private String name;
 	private int charge;
+	public final boolean isPolyatomic;
 	
 	private Anion(Element baseElement, String ionicName, int ionCharge)
 	{
@@ -20,6 +24,7 @@ public enum Anion implements Ion
 		
 		name = ionicName;
 		charge = ionCharge;
+		isPolyatomic = false;
 		
 		if (baseElement != null) baseElement.addIon(this);
 	}
@@ -34,6 +39,7 @@ public enum Anion implements Ion
 		
 		name = ionicName;
 		charge = ionCharge;
+		isPolyatomic = true;
 	}
 	
 	@Override
