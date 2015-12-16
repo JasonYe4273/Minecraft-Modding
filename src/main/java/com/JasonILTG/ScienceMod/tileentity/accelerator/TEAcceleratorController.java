b57c04af57bcab3a7c33dbdb9db35d316bb2bad2
Jack <Jack@SYY_SP3>
2015-12-16 15:21:43 -0800

    Working on particle accelerator

--- src/main/java/com/JasonILTG/ScienceMod/tileentity/accelerator/TEAcceleratorController.java
+++ src/main/java/com/JasonILTG/ScienceMod/tileentity/accelerator/TEAcceleratorController.java
@@ -1,21 +1,32 @@
 package com.JasonILTG.ScienceMod.tileentity.accelerator;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.gui.IUpdatePlayerListBox;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
 
 import com.JasonILTG.ScienceMod.manager.power.PowerManager;
 import com.JasonILTG.ScienceMod.tileentity.general.ITileEntityPowered;
 
-public class TEAcceleratorController extends TEAccelerator implements ITileEntityPowered
+public class TEAcceleratorController extends TEAccelerator implements ITileEntityPowered, IUpdatePlayerListBox
 {
 	private static final String NAME = NAME_PREFIX + "Controller";
 	private static final int DEFAULT_POWER_DRAIN = 100;
+	private static final int MAX_CHARGE_TIME = 200;
 	
 	private PowerManager power;
 	private int powerPerTick;
+	
+	private int maxCharge;
+	private int currentCharge;
+	private boolean isActive;
+	
 	private boolean isFormed;
 	
-	public TEAcceleratorController()
+	public TEAcceleratorController(World worldIn, BlockPos position)
 	{
+		super(worldIn, position);
+		
 		isFormed = false;
 		powerPerTick = DEFAULT_POWER_DRAIN;
 	}
@@ -28,6 +39,18 @@ public class TEAcceleratorController extends TEAccelerator implements ITileEntit
 	public void dismantle()
 	{
 		isFormed = false;
+	}
+	
+	@Override
+	public void update()
+	{
+		// Do action only when formed and active
+		if (!isFormed || !isActive) return;
+		
+		if (hasPower())
+		{	
+			
+		}
 	}
 	
 	@Override
