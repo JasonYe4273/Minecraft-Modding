9270d096257ae68178f981dbebe3cea237dfe37c
Jason <jasoniltg@gmail.com>
2015-12-19 00:36:03 -0800

    Whoops

--- src/main/java/com/JasonILTG/ScienceMod/init/ScienceModItems.java
+++ src/main/java/com/JasonILTG/ScienceMod/init/ScienceModItems.java
@@ -104,7 +104,7 @@ public class ScienceModItems
 	private static void addVariants(ItemScience item)
 	{
 		// Check that the item has subtypes
-		if (!item.getHasSubtypes()) return;
+		if (item.getNumSubtypes() == 1) return;
 		
 		for (int meta = 0; meta < item.getNumSubtypes(); meta ++)
 		{
