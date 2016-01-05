665949e8dc8af5d211f464b67f067a4d22da187c
syy1125 <syy1125@hotmail.com>
2016-01-04 19:05:21 -0800

    ???

--- src/main/java/com/JasonILTG/ScienceMod/handler/item/ExoHandler.java
+++ src/main/java/com/JasonILTG/ScienceMod/handler/item/ExoHandler.java
@@ -1,13 +1,10 @@
 package com.JasonILTG.ScienceMod.handler.item;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import com.JasonILTG.ScienceMod.item.armor.Exoskeleton;
 
 /**
  * Handles exo armor.
@@ -22,42 +19,16 @@ public class ExoHandler
 	
 	public static void init()
 	{
-		MinecraftForge.EVENT_BUS.register(ExoHandler.instance);
+		MinecraftForge.EVENT_BUS.register(instance);
 		FMLCommonHandler.instance().bus().register(instance);
 	}
 	
 	@SubscribeEvent
-	public void onPlayerTick(TickEvent.PlayerTickEvent event)
+	public void onPlayerTick(LivingUpdateEvent event)
 	{
-		applySpeed(event.player);
-		applyFall(event.player);
-	}
-	
-	/**
-	 * Applies a speed increase for players wearing exoskeleton. Not working currently.
-	 * 
-	 * @param player The player to apply the effect to
-	 */
-	private void applySpeed(EntityPlayer player)
-	{
-		ItemStack leggings = player.inventory.armorInventory[2];
-		if (leggings.getItem() instanceof Exoskeleton) {
-			player.capabilities.setPlayerWalkSpeed(0.12f);
-		}
-		else {
-			player.capabilities.setPlayerWalkSpeed(0.1f);
-		}
-	}
-	
-	/**
-	 * Nullifies fall damage for players wearing exoskeleton. Not tested.
-	 * 
-	 * @param player The player to reduce fall damage
-	 */
-	private void applyFall(EntityPlayer player)
-	{
-		if (player.inventory.armorInventory[3].getItem() instanceof Exoskeleton && !player.capabilities.allowFlying) {
-			player.fallDistance -= player.fallDistance;
-		}
+		// Only operate on players
+		if (!(event.entityLiving instanceof EntityPlayer)) return;
+		EntityPlayer player = (EntityPlayer) event.entityLiving;
+		
 	}
 }
