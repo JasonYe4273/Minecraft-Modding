91e3f7b38b809d92c210f810237122d40ceda96a
Jack <Jack@10.63.116.80>
2016-01-06 09:11:00 -0800

    Still trying to figure out how to apply speed bonus

--- src/main/java/com/JasonILTG/ScienceMod/handler/item/ExoHandler.java
+++ src/main/java/com/JasonILTG/ScienceMod/handler/item/ExoHandler.java
@@ -1,10 +1,13 @@
 package com.JasonILTG.ScienceMod.handler.item;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.JasonILTG.ScienceMod.item.armor.Exoskeleton;
 
 /**
  * Handles exo armor.
@@ -29,5 +32,17 @@ public class ExoHandler
 		// Only operate on players
 		if (!(event.entityLiving instanceof EntityPlayer)) return;
 		EntityPlayer player = (EntityPlayer) event.entityLiving;
+		
+		updateSpeed(player);
+	}
+	
+	private void updateSpeed(EntityPlayer player)
+	{
+		Item legs = player.inventory.armorItemInSlot(2).getItem();
+		if (legs instanceof Exoskeleton)
+		{
+			float extraSpeed = ((Exoskeleton) legs).getExtraSpeed();
+			player.moveEntity(player.motionX * extraSpeed, player.motionY * extraSpeed, player.motionZ * extraSpeed);
+		}
 	}
 }
