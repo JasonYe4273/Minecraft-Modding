12632d52bd5e70080056aed266efb20e11baa002
Jason <jasoniltg@gmail.com>
2016-03-04 20:50:05 -0800

    Small Change

--- src/main/java/com/JasonILTG/ScienceMod/handler/ScienceHandlers.java
+++ src/main/java/com/JasonILTG/ScienceMod/handler/ScienceHandlers.java
@@ -1,11 +1,10 @@
 package com.JasonILTG.ScienceMod.handler;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-
 import com.JasonILTG.ScienceMod.handler.item.ArmorHandler;
 import com.JasonILTG.ScienceMod.handler.item.ExoHandler;
 import com.JasonILTG.ScienceMod.handler.manager.ManagerHandler;
+
+import net.minecraftforge.common.MinecraftForge;
 
 public class ScienceHandlers
 {
@@ -15,10 +14,5 @@ public class ScienceHandlers
 		MinecraftForge.EVENT_BUS.register(ArmorHandler.instance);
 		MinecraftForge.EVENT_BUS.register(ScienceEventHandler.instance);
 		MinecraftForge.EVENT_BUS.register(ManagerHandler.instance);
-		
-		FMLCommonHandler.instance().bus().register(ExoHandler.instance);
-		FMLCommonHandler.instance().bus().register(ArmorHandler.instance);
-		FMLCommonHandler.instance().bus().register(ScienceEventHandler.instance);
-		FMLCommonHandler.instance().bus().register(ManagerHandler.instance);
 	}
 }
